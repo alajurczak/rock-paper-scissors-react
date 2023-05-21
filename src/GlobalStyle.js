b492712c25d0aa4023ce08c6389dae39e2075bea
Alicja Jurczak <ala.jurczak@gmail.com>
2023-05-21 22:22:49 +0200

    GlobalStyle -  body styles added

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -12,5 +12,9 @@ export const GlobalStyle = createGlobalStyle`
     body {
         margin: 0 auto;
         word-break: break-word;
+        background: radial-gradient(at top,hsl(214, 47%, 23%), hsl(237, 49%, 15%));
+        min-height: 100vh;
+        min-width: 100vw;
+        font-family: "Barlow Semi Condensed", sans-serif;
     }
 `;
