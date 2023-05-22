36c17877864db39907edd02bb99a31c6b4d72a77
Alicja Jurczak <ala.jurczak@gmail.com>
2023-05-22 20:22:54 +0200

    icons added and padding at GlobalStyle added

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -15,6 +15,7 @@ export const GlobalStyle = createGlobalStyle`
         background: radial-gradient(at top,hsl(214, 47%, 23%), hsl(237, 49%, 15%));
         min-height: 100vh;
         min-width: 100vw;
+        padding-top: 80px;
         font-family: "Barlow Semi Condensed", sans-serif;
     }
 `;
